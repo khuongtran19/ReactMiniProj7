44f3cf0f68a8dac67ba3fee093097215725c5ca5
khuong <tranvinhkhuong@gmail.com>
2020-07-07 16:35:50 -0700

    update

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
-    renderError({ error }) {
+    renderError({ error, touched }) {
         if (touched && error) {
             return (
                 <div className='ui error message'>
@@ -12,8 +12,9 @@ class StreamCreate extends React.Component {
         }
     }
     renderInput = ({ input, label, meta }) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return (
-            <div className='field'>
+            <div className={className}>
                 <label>{label}</label>
                 <input {...input} autoComplete='off' />
                 {this.renderError(meta)}
@@ -30,7 +31,7 @@ class StreamCreate extends React.Component {
             // this.props.handleSubmit <- provide by redux form
             <form
                 onSubmit={this.props.handleSubmit(this.onSubmit)}
-                className='ui form'>
+                className='ui form error'>
                 <Field
                     name='title'
                     component={this.renderInput}
